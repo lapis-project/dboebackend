c89ec1b1234462f2dbb010aa90c1f415cc640531
csae8092 <p.andorfer@gmail.com>
2026-01-15 14:01:47 +0100

    chore: updated fixtures

--- create_fixtures.sh
+++ create_fixtures.sh
@@ -3,12 +3,14 @@
 
 # make sure you have django-fixture-magic installed and registered in installed app
 
+# after creating the fixtures, remove all  contenttypes.contenttype and auth.permission objects
+
 
 # echo "create fixtures_collection"
 # uv run manage.py dump_object annotations.collection 36 > fixtures_collection.json
 
 # echo "create fixtures_belege"
-# uv run manage.py dump_object belege.beleg f243_qdb-d1e29146 > fixtures_belege.json
+# uv run manage.py dump_object belege.beleg f243_qdb-d1e29146 e224_qdb-d1e65954 d198_qdbn-d16e17226 > fixtures_belege.json
 
 # echo "merging fixturs"
 # uv run manage.py merge_fixtures fixtures_collection.json fixtures_belege.json > belege/fixtures/dump.json
