f09d513370b991227765c24d9502b91e84b38a19
csae8092 <p.andorfer@gmail.com>
2026-01-15 13:12:35 +0100

    feat: add more stats endpoints

--- stats/urls.py
+++ stats/urls.py
@@ -5,7 +5,13 @@ from stats import views
 app_name = "belege"
 
 urlpatterns = [
-    path("belege-by-collection/", views.beleg_by_collection),
+    path("belege-by-anmerkung-lautung/", views.beleg_by_note_lautung_count),
+    path("belege-by-bedeutung/", views.beleg_by_sense_count),
+    path("belege-by-collection/", views.beleg_by_collection_count),
+    path("belege-by-context/", views.beleg_by_context_count),
+    path("belege-by-facs/", views.beleg_by_facs_count),
+    path("belege-by-lautung/", views.beleg_by_lautung_count),
+    path("belege-by-lehnwort/", views.beleg_by_lehnwort_count),
     path("collection-by-beleg/", views.collection_by_beleg_count),
-    path("tags-by-beleg/", views.tag_by_beleg),
+    path("tags-by-beleg/", views.tag_by_beleg_count),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, F
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,38 +9,107 @@ from belege.models import Beleg
 
 @extend_schema(responses=dict)
 @api_view(["get"])
-def collection_by_beleg_count(request):
-    data = (
-        Collection.objects.annotate(item_count=Count("beleg"))
+def beleg_by_facs_count(request):
+    payload = list(
+        Beleg.objects.annotate(item_count=Count("facs"))
+        .filter(item_count__gt=0)
         .order_by("-item_count")[:25]
-        .values_list("id", "title", "item_count")
+        .values(id=F("dboe_id"), value=F("hauptlemma"), item_count=F("item_count"))
     )
-    payload = [{"id": x[0], "value": x[1], "item_count": x[2]} for x in data]
+    return Response({"title": "Belege nach Faksimiles)", "payload": payload})
 
+
+@extend_schema(responses=dict)
+@api_view(["get"])
+def beleg_by_note_lautung_count(request):
+    payload = list(
+        Beleg.objects.annotate(item_count=Count("note_lautung"))
+        .filter(item_count__gt=0)
+        .order_by("-item_count")[:25]
+        .values(id=F("dboe_id"), value=F("hauptlemma"), item_count=F("item_count"))
+    )
+    return Response({"title": "Belege nach Anmerkungen(Lautung)", "payload": payload})
+
+
+@extend_schema(responses=dict)
+@api_view(["get"])
+def beleg_by_sense_count(request):
+    payload = list(
+        Beleg.objects.annotate(item_count=Count("bedeutungen"))
+        .filter(item_count__gt=0)
+        .order_by("-item_count")[:25]
+        .values(id=F("dboe_id"), value=F("hauptlemma"), item_count=F("item_count"))
+    )
+    return Response({"title": "Belege nach Bedeutungen", "payload": payload})
+
+
+@extend_schema(responses=dict)
+@api_view(["get"])
+def beleg_by_lehnwort_count(request):
+    payload = list(
+        Beleg.objects.annotate(item_count=Count("lehnwoerter"))
+        .filter(item_count__gt=0)
+        .order_by("-item_count")[:25]
+        .values(id=F("dboe_id"), value=F("hauptlemma"), item_count=F("item_count"))
+    )
+    return Response({"title": "Belege nach Lehnwörtern", "payload": payload})
+
+
+@extend_schema(responses=dict)
+@api_view(["get"])
+def beleg_by_lautung_count(request):
+    payload = list(
+        Beleg.objects.annotate(item_count=Count("lautungen"))
+        .filter(item_count__gt=0)
+        .order_by("-item_count")[:25]
+        .values(id=F("dboe_id"), value=F("hauptlemma"), item_count=F("item_count"))
+    )
+    return Response({"title": "Belege nach Lautungen", "payload": payload})
+
+
+@extend_schema(responses=dict)
+@api_view(["get"])
+def beleg_by_context_count(request):
+    payload = list(
+        Beleg.objects.annotate(item_count=Count("citations"))
+        .filter(item_count__gt=0)
+        .order_by("-item_count")[:25]
+        .values(id=F("dboe_id"), value=F("hauptlemma"), item_count=F("item_count"))
+    )
+    return Response({"title": "Belege nach Kontexten", "payload": payload})
+
+
+@extend_schema(responses=dict)
+@api_view(["get"])
+def collection_by_beleg_count(request):
+    payload = list(
+        Collection.objects.annotate(item_count=Count("beleg"))
+        .filter(item_count__gt=0)
+        .order_by("-item_count")[:25]
+        .values(id=F("id"), value=F("title"), item_count=F("item_count"))
+    )
     return Response({"title": "Collection nach Belegen", "payload": payload})
 
 
 @extend_schema(responses=dict)
 @api_view(["get"])
-def beleg_by_collection(request):
-    data = (
+def beleg_by_collection_count(request):
+    payload = list(
         Beleg.objects.annotate(item_count=Count("collection"))
+        .filter(item_count__gt=0)
         .order_by("-item_count")[:25]
-        .values_list("dboe_id", "hauptlemma", "item_count")
+        .values(id=F("dboe_id"), value=F("hauptlemma"), item_count=F("item_count"))
     )
-    payload = [{"id": x[0], "value": x[1], "item_count": x[2]} for x in data]
-
     return Response({"title": "Belege nach Collections", "payload": payload})
 
 
 @extend_schema(responses=dict)
 @api_view(["get"])
-def tag_by_beleg(request) -> dict:
-    data = (
+def tag_by_beleg_count(request) -> dict:
+    payload = list(
         Tag.objects.annotate(item_count=Count("belege"))
+        .filter(item_count__gt=0)
         .order_by("-item_count")[:25]
-        .values_list("id", "name", "item_count")
+        .values(id=F("id"), value=F("name"), item_count=F("item_count"))
     )
-    payload = [{"id": x[0], "value": x[1], "item_count": x[2]} for x in data]
-
     return Response({"title": "Tags nach Belegen", "payload": payload})
